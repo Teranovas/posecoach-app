8ec6146c8566c0623c7080a2eaf75f3cf0a429c4
Teranovas <goteb123@hs.ac.kr>
2025-08-15 18:07:31 +0900

    gradle 모듈 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,14 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        // 로컬 서버 URL을 BuildConfig에 넣어 사용 (에뮬레이터: 10.0.2.2)
-        buildConfigField "String", "POSE_SERVER_BASE_URL", "\"http://10.0.2.2:5001/\"
+
+        // Kotlin DSL에서는 인자들을 문자열로 넘겨야 합니다.
+        // value에는 이스케이프된 따옴표 포함: "\"http://10.0.2.2:5001/\""
+        buildConfigField(
+            "String",
+            "POSE_SERVER_BASE_URL",
+            "\"http://10.0.2.2:5001/\""
+        )
     }
 
     buildTypes {
@@ -27,7 +33,16 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            // 필요시 디버그 전용 세팅
+        }
     }
+
+    // 뷰바인딩 사용(예제 코드에서 viewBinding 씀)
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -38,21 +53,22 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation "com.squareup.retrofit2:retrofit:2.11.0"
-    implementation "com.squareup.retrofit2:converter-gson:2.11.0"
-    implementation "com.squareup.okhttp3:okhttp:4.12.0"
-    implementation "com.squareup.okhttp3:logging-interceptor:4.12.0"
+    // Retrofit + OkHttp + Gson
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     // 이미지 로딩(선택)
-    implementation "io.coil-kt:coil:2.6.0"
+    implementation("io.coil-kt:coil:2.6.0")
 }
